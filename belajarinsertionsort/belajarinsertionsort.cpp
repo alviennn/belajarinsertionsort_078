a3656e5bbaefd134ed3ecd6b351514776a742198
alviennn <alvien.rnp@gmail.com>
2023-03-24 16:05:05 +0700

    menambahkan perbandingan, temp, perulangan

--- belajarinsertionsort/belajarinsertionsort.cpp
+++ belajarinsertionsort/belajarinsertionsort.cpp
@@ -32,4 +32,23 @@ void insertionsort() {
 	int temp;
 	int j;
 
+	for (int i = 1; i < n; i++) {
+
+		temp = arr[i];
+
+		j = i - 1;
+
+		while (j >= 0 && arr[j > temp]); {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = temp;
+
+		cout << "\npass" << i << ": ";
+		for (int k = 0; k < n; k++) {
+			cout << arr[k] << " ";
+		}
+	}
 }
+
+
