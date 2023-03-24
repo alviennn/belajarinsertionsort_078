177e485a41285843e073d5abe9a2ee97ed7e3771
alviennn <alvien.rnp@gmail.com>
2023-03-24 16:06:08 +0700

    menambahkan prosedur display dengan tipe void

--- belajarinsertionsort/belajarinsertionsort.cpp
+++ belajarinsertionsort/belajarinsertionsort.cpp
@@ -51,4 +51,14 @@ void insertionsort() {
 	}
 }
 
+void display() {
+	cout << "\n===============================" << endl;
+	cout << "Element Array Yang Telah Tersusun" << endl;
+	cout << "=================================" << endl;
+
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << " ";
+	}
+}
+
 
