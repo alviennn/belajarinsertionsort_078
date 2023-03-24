779cee142deb1e9e03d4e366ecdc6d42e896f367
alviennn <alvien.rnp@gmail.com>
2023-03-24 16:01:57 +0700

    menambahkan perulangan

--- belajarinsertionsort/belajarinsertionsort.cpp
+++ belajarinsertionsort/belajarinsertionsort.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int array[20];
+int arr[20];
 int n;
 
 void input() {
 	while (true) {
-		cout << "masukkan jumlah data pada array";
+		cout << "masukkan jumlah data pada array : ";
 		cin >> n;
 
 		if (n <= 20) {
 			break;
 		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
 	}
 }
+
